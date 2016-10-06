2e5af01c7cd2d0f10c36088d215edceb79157d49
thejohnhoffer <hoffexi@gmail.com>
2016-10-06 11:09:02 -0400

    hdf5 supports two layers

--- butterfly/hdf5.py
+++ butterfly/hdf5.py
@@ -27,32 +27,40 @@ class HDF5DataSource(DataSource):
     '''
     
     def __init__(self, core, datapath, dtype=np.uint8):
-        d = json.load(open(datapath, "r"))
-        self.hdf5_file = d[K_FILENAME]
-        self.data_path = d[K_DATASET_PATH]
+        layers = json.load(open(datapath, "r"))
+        if type(layers) is not list:
+            layers = [layers]
+        self.layers = len(layers)
+        self.hdf5_file = [d[K_FILENAME] for d in layers]
+        self.data_path = [d[K_DATASET_PATH] for d in layers]
         super(HDF5DataSource, self).__init__(core, datapath)
     
     def index(self):
-        with h5py.File(self.hdf5_file, "r") as fd:
-            self.blocksize = fd[self.data_path].shape[1:]
+        with h5py.File(self.hdf5_file[0], "r") as fd:
+            self.blocksize = fd[self.data_path[0]].shape[1:]
         return
     
     def load_cutout(self, x0, x1, y0, y1, z, w):
-        with h5py.File(self.hdf5_file, "r") as fd:
-            ds = fd[self.data_path]
+        with h5py.File(self.hdf5_file[0], "r") as fd:
+            ds = fd[self.data_path[0]]
             stride = 2 ** w
             return ds[z, y0:y1:stride, x0:x1:stride]
 
     def load(self, x, y, z, w, segmentation):
-        (bx,by) = self.blocksize
-        return self.load_cutout(x, x+bx, y, y+by, z, w)
+        layerIndex = int(segmentation)%self.layers
+        with h5py.File(self.hdf5_file[layerIndex], "r") as fd:
+            stride = 2 ** w
+            ds = fd[self.data_path[layerIndex]]
+            (bx,by) = self.blocksize
+            return ds[z, y:y+by:stride, x:x+bx:stride]
 
     def seg_to_color(self, slice):
         # Simply expand all 32 bit ids into four 8 bit ints
+        slice = slice.astype(np.uint32)
         colors = slice.view(np.uint8).reshape(slice.shape + (4,))
         return colors[:,:,:3]
     
     def get_boundaries(self):
-        with h5py.File(self.hdf5_file, "r") as fd:
-            ds = fd[self.data_path]
+        with h5py.File(self.hdf5_file[0], "r") as fd:
+            ds = fd[self.data_path[0]]
             return (ds.shape[2], ds.shape[1], ds.shape[0])
