042c27f07479082bf7b774c7ae0ce2b6f21e70bf
thejohnhoffer <hoffexi@gmail.com>
2016-10-17 10:45:42 -0400

    add descriptions

--- butterfly/static/index/write.js
+++ butterfly/static/index/write.js
@@ -15,18 +15,24 @@ DOJO.Write.prototype = {
     'img':'',
     'ids':'&id'
   },
-  copy: function(id){
-    return document.getElementById(id).cloneNode(1);
+  copy: function(id,i){
+    return document.getElementById(id).children[i].cloneNode(1);
   },
-  grandkid: function(el){
-    return el.children[1].children[1];
+  grandparent: function(el){
+    return el.parentElement.parentElement;
+  },
+  grandkid: function(el,A){
+    return A.reduce(function(el,i){
+      return el.children[i];
+    },el);
   },
   head: function(source){
-      var self = this.copy('path');
       var parent = document.getElementById(source.old);
-      var cousin = this.grandkid(parent.parentElement);
-      var offspring = this.grandkid(self);
-      self.removeAttribute('id');
+      var grandparent = parent.parentElement;
+      var cousin = this.grandkid(grandparent,[1,1]);
+
+      var self = this.copy('proto',0);
+      var offspring = this.grandkid(self,[1,1]);
       var id = this.totalIds++;
       var path = [
         ['id', 'in'+id],
@@ -43,16 +49,30 @@ DOJO.Write.prototype = {
   },
   body: function(source){
       var parent = document.getElementById(source.old);
-      var cousin = this.grandkid(parent.parentElement);
-      var ancestor = parent.parentElement.parentElement.parentElement;
-      var uncle = this.grandkid(ancestor);
+      var grandparent = parent.parentElement;
+      var cousin = this.grandkid(grandparent,[1,1]);
+
+      var ancestor = this.grandparent(grandparent);
+      var uncle = this.grandkid(ancestor,[1,1]);
       var [w,h,d] = [source.dimensions.x,source.dimensions.y,source.dimensions.z];
       var path = 'viz.html?datapath='+source.path+'&width='+w+'&height='+h;
       cousin.children[0].href = path+this.channels[source.name];
       cousin.children[1].innerHTML = source['data-type'];
       uncle.children[1].innerHTML = [w,h,d].join(', ');
       uncle.children[0].href = path+'&overlay';
+      grandparent.children[0].checked = false;
       ancestor.children[0].checked = false;
+      var path = [
+        ['name', source['short-description']],
+        ['path', source.path]
+      ]
+      path.forEach(function(items){
+        var temp = this.copy('proto',2);
+        var info = this.grandkid(temp,[0,0]);
+        info.children[0].innerHTML = items[0];
+        info.children[1].innerHTML = items[1];
+        parent.appendChild(temp);
+      },this);
   },
   main: function(terms){
     var source = this.share(terms,{now:this.argue(terms)});
