aab4f7a68929e221bf74a991300243103d0344f8
thejohnhoffer <hoffexi@gmail.com>
2016-10-26 16:00:29 -0400

    highlighting works

--- butterfly/static/viz/realtime.js
+++ butterfly/static/viz/realtime.js
@@ -27,47 +27,38 @@ DOJO.RealTime.prototype = {
         var isDojo = fun.call.bind(function(){
           return this.source && this.source.dojo;
         });
-        var isTile = function(level,tile){
-          var where = new OpenSeadragon.Point(tile.x,tile.y);
-          return tile.level == level && this.equals(where);
+        var isTile = function(tile){
+          var here = this.bounds.getTopLeft();
+          var where = tile.bounds.getTopLeft();
+          return this.level == tile.level && here.equals(where);
         }
         // Image to Tile
         var image2tile = function(point,space,shape) {
             var d = point.minus(space);
-            log(d)
-            log(shape)
-            log(' ')
             return [d.x/shape.x, d.y/shape.y];
         }
         var click = function(callback,e){
 
           var allItems = stack.getItems('now').reverse();
           var point = stack.vp.viewerElementToViewportCoordinates(e.position);
+          var layer = allItems.filter(canClick)[0];
           var dojo = allItems.filter(isDojo)[0];
-          if(!dojo){
+          if(!dojo || !layer){
             return;
           }
-          var last = dojo.lastDrawn[0];
-          for (var layer of allItems.filter(canClick)){
-            if (last){
-              var level = last.level;
-              var xy = dojo.source.getTileAtPoint(level,point);
-              var toTile = dojo.lastDrawn.filter(isTile.bind(xy,level))[0];
-              var fromTile = layer.lastDrawn.filter(isTile.bind(xy,level))[0];
-              if (fromTile && toTile){
-
-                e.rendered = fromTile.cacheImageRecord._renderedContext;
-                e.output = toTile.cacheImageRecord._renderedContext;
-
-                var tileShape = fromTile.bounds.getSize();
-                var tileSpace = fromTile.bounds.getTopLeft();
-                this.viaGL.clickSpot = image2tile(point,tileSpace,tileShape);
-                this.viaGL.clickSpot[1] = 1 - this.viaGL.clickSpot[1];
-                callback(e);
-                break;
-              }
+          for (var toTile of dojo.lastDrawn){
+            var fromTile = layer.lastDrawn.filter(isTile.bind(toTile))[0];
+            if (fromTile){
+              e.rendered = fromTile.cacheImageRecord.getRenderedContext();
+              e.output = toTile.cacheImageRecord.getRenderedContext();
+              var tileShape = fromTile.bounds.getSize();
+              var tileSpace = fromTile.bounds.getTopLeft();
+              this.viaGL.clickSpot = image2tile(point,tileSpace,tileShape);
+              this.viaGL.clickSpot[1] = 1 - this.viaGL.clickSpot[1];
+              callback(e);
             }
           }
+          this.openSD.forceRedraw();
         }
 
         // Load for glsl
