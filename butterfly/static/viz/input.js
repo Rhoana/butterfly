e890f247133c887086bcf07ebd364e7ddda10ad9
thejohnhoffer <hoffexi@gmail.com>
2016-11-14 13:52:01 -0500

    clearer naming

--- butterfly/static/viz/input.js
+++ butterfly/static/viz/input.js
@@ -22,7 +22,7 @@ DOJO.Input = function(scope) {
     this.stack = scope.stack;
     this.realT = new DOJO.RealTime(scope);
     this.realT.init(this).then(this.init.bind(this));
-    this.findings[0].childNodes[0].innerHTML = this.stack.now;
+    this.findings[0].childNodes[0].innerHTML = this.stack.z;
     this.findings[0].childNodes[1].innerHTML = '/'+(this.stack.depth-1);
 }
 
@@ -72,15 +72,15 @@ DOJO.Input.prototype = {
     up: function(stack){
         stack.showLayer(1);
         stack.hideLayer(0);
-        stack.now ++;
+        stack.z ++;
         stack.zBuff = stack.updateBuff(stack.zBuff,'up');
-        this.findings[0].childNodes[0].innerHTML = stack.now;
+        this.findings[0].childNodes[0].innerHTML = stack.z;
     },
     down: function(stack){
         stack.showLayer(-1);
         stack.hideLayer(0);
-        stack.now --;
+        stack.z --;
         stack.zBuff = stack.updateBuff(stack.zBuff,'down');
-        this.findings[0].childNodes[0].innerHTML = stack.now;
+        this.findings[0].childNodes[0].innerHTML = stack.z;
     }
 }
