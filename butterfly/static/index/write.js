474dadb01dfbcb07131cc95fb79cb83532d244f7
thejohnhoffer <hoffexi@gmail.com>
2017-01-19 10:02:19 -0500

    allow float images

--- butterfly/static/index/write.js
+++ butterfly/static/index/write.js
@@ -44,8 +44,8 @@ DOJO.Write.prototype = {
     var uncle = this.grandkid(ancestor,[1,1]);
     var size = source.dimensions;
     var dtype = source['data-type'];
-    var withGL = Number(dtype!='uint8');
     var [w,h,d] = [size.x,size.y,size.z];
+    var withGL = !dtype.match(/float|uint8/);
     var channel = withGL + source.channel;
     var path = 'viz.html?depth='+d+'&width='+w+'&height='+h;
     var old = source.old.replace(/&channel=([^&]+)/,'&channel='+channel);;
