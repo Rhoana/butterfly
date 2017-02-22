4cd596a2caf6f83785602f0ddd497c034df9f422
thejohnhoffer <hoffexi@gmail.com>
2017-02-21 23:42:21 -0500

    better documentation generation

--- scripts/settings2yaml.py
+++ scripts/settings2yaml.py
@@ -1,12 +1,37 @@
+import os
+import re
 import yaml
+import numpy as np
 from Settings import *
-data = {
-    'INPUT': INPUT(),
-    'RUNTIME': RUNTIME(),
-    'OUTPUT': OUTPUT()
+yaml_setup = {
+    'default_flow_style': False
 }
-with open('Settings.yaml', 'w') as outfile:
-    yaml.dump(data, outfile, default_flow_style=False)
+np_setup = {
+    'fmt': '%s',
+    'delimiter': ' | '
+}
 
+def class2yaml(classy):
+    unclean = yaml.dump(classy(), **yaml_setup)
+    clean = re.sub('!!python.+\n','\n',unclean)
+    return clean.split('\n')
 
-    
+def pad_lists(lists):
+    max_lines = len(max(lists,key=len))
+    c_shape = (max_lines, len(lists))
+    max_chars = 0
+    for li in lists:
+        max_chars = max(max_chars, len(max(li,key=len)))
+        pad_lines = max_lines - len(li)
+        li += [''] * pad_lines
+    # Create empty numpy array for the text lists
+    ch_a = np.chararray(c_shape,itemsize=max_chars)
+    ch_a[:] = zip(*lists)
+    return ch_a
+
+line_lists = map(class2yaml, [INPUT,OUTPUT,RUNTIME])
+char_ar = pad_lists(line_lists)
+
+with open('Settings.csv', 'w') as outfile:
+    padded_lists = char_ar.ljust(char_ar.itemsize, ' ')
+    np.savetxt(outfile, padded_lists, **np_setup)
