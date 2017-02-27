06024f371891e5eb1ee6744c87c4536237343646
Daniel Haehn <haehn@seas.harvard.edu>
2017-02-27 14:09:53 -0500

    Simplify coordinate system.

--- butterfly/CoreLayer/Core.py
+++ butterfly/CoreLayer/Core.py
@@ -32,16 +32,17 @@ class Core(object):
             tile_crop = query.all_in_some(t_index)
             t_query = TileQuery(query, t_index, tile_crop)
             tile = self.load_tile(query, t_query)
+
             # Fill the tile into the full cutout
             to_cut = [t_query.target_origin, tile.shape]
             [Z0,Y0,X0],[Z1,Y1,X1] = query.some_in_all(*to_cut)
             cutout[Z0:Z1,Y0:Y1,X0:X1] = tile
 
-        return np.moveaxis(cutout,0,-1)
+        return cutout
 
     def load_tile(self, query, t_query):
         # grab request size for query
-        (K0,J0,I0),(K1,J1,I1) = t_query.target_bounds
+        (K0,J0,I0),(K1,J1,I1) = t_query.target_bounds-t_query.target_origin
 
         # Load from cache or from disk if needed
         cache_tile = self._cache.get_tile(query,t_query)
@@ -52,6 +53,7 @@ class Core(object):
 
         tile = source_class.load_tile(t_query)
         self._cache.add_tile(query,t_query,tile)
+
         return tile[K0:K1,J0:J1,I0:I1]
 
     def write_image(self, query, vol):
@@ -60,13 +62,13 @@ class Core(object):
 
         if img_format.VALUE in img_format.ZIP_LIST:
             output = StringIO.StringIO()
-            volstring = vol[:,:,0].T.tostring('F')
+            volstring = vol[0,:,:].T.tostring('F')
             output.write(zlib.compress(volstring))
             return output.getvalue()
 
         if img_format.VALUE in img_format.TIF_LIST:
             output = StringIO.StringIO()
-            tiffvol = vol[:,:,0]
+            tiffvol = vol[0,:,:]
             tifffile.imsave(output, tiffvol)
             return output.getvalue()
 
