50a50567599496f859d090a7a74c4d82627bab69
thejohnhoffer <hoffexi@gmail.com>
2017-02-28 22:18:37 -0500

    use least-recently used cache!

--- butterfly/CoreLayer/Cache.py
+++ butterfly/CoreLayer/Cache.py
@@ -1,23 +1,35 @@
 import logging
 from pylru import lrucache
 from Settings import MAX_CACHE_ENTRY
-from Settings import MAX_CACHE_SIZE
 from CacheEntry import CacheEntry
 
 class Cache(object):
-    max_size = MAX_CACHE_SIZE
     max_entry = MAX_CACHE_ENTRY
 
     def __init__(self):
+        self.n_entry = 0
         self._sources = lrucache(self.max_entry)
+
+    def update_size(self,key):
+        entry = self._sources.peek(key)
+        # update the max entries for the entry
+        max_entry = entry.update_size(self.n_entry)
+        self.log('update', key=key, n=max_entry)
 
     def get_source(self, query):
         return self._sources.get(query.key, 0)
 
     def add_source(self, query):
         if not self.get_source(query):
-            self._sources[query.key] = CacheEntry(query)
+            self.n_entry = min(self.n_entry+1, self.max_entry)
+            new_entry = CacheEntry(query, self.n_entry)
+            # Add a new entry to the cache entries
+            self._sources[query.key] = new_entry
             self.log('add_source',src=query.key)
+            if self.n_entry < self.max_entry:
+                # Lower the max size for each cache added
+                self.log('n_entry', n_entry=self.n_entry)
+                map(self.update_size, self._sources)
         return self.get_source(query)
 
     def add_tile(self, query, t_query, content):
@@ -36,11 +48,15 @@ class Cache(object):
     def log(self, action, **kwargs):
         statuses = {
             'add_source': 'info',
-            'add_tile': 'info'
+            'add_tile': 'info',
+            'n_entry': 'info',
+            'update': 'info'
         }
         actions = {
             'add_source': 'Starting {src} cache',
-            'add_tile': 'Adding {id} to {src} cache'
+            'add_tile': 'Adding {id} to {src} cache',
+            'n_entry': 'Total {n_entry} entries in cache',
+            'update': 'Entry {key} can have {n} entries'
         }
         status = statuses[action]
         message = actions[action].format(**kwargs)
--- butterfly/CoreLayer/CacheEntry.py
+++ butterfly/CoreLayer/CacheEntry.py
@@ -1,9 +1,17 @@
-import pylru
+from Settings import MAX_CACHE_SIZE
+from pylru import lrucache
+import numpy as np
 
 class CacheEntry(object):
-    def __init__(self, query):
-        self._tiles = {}
+    max_size = MAX_CACHE_SIZE
+    def __init__(self, query, n_entry):
+        each_size = np.prod(query.blocksize)
+        each_bytes = query.dtype(0).itemsize
+        self.each = each_size*each_bytes
         self.query = query
+        # update max entries and create tile cache
+        max_entry = self.update_size(n_entry)
+        self._tiles = lrucache(max_entry)
 
     @property
     def loaded_source(self):
@@ -14,6 +22,13 @@ class CacheEntry(object):
             runtime.BLOCK.NAME: runtime.BLOCK.VALUE,
             output.SIZE.NAME: output.SIZE.VALUE
         }
+
+    def update_size(self, n_entry):
+        my_max_size = self.max_size/n_entry
+        max_entry = int(my_max_size/self.each)
+        if hasattr(self,'_tiles'):
+            self._tiles.size(max_entry)
+        return max_entry
 
     def add_tile(self, t_query, content):
         self._tiles[t_query.key] = content
