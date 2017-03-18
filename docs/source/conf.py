04246eedcb2b202346a6538d6a9044ec6fbc064f
John Hoffer <hoffexi@gmail.com>
2017-03-18 15:38:30 -0400

    change sys.path in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../butterfly'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
