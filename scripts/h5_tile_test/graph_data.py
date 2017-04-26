e537aaf29d77f682369430ca7aad18ff1691299e
thejohnhoffer <hoffexi@gmail.com>
2017-04-26 10:38:21 -0400

    graph h5_tile_test results correctl

--- scripts/h5_tile_test/graph_data.py
+++ scripts/h5_tile_test/graph_data.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 from glob import glob1
 import numpy as np
 import json
+import os
 
 if __name__ == '__main__':
 
     # Everything happens from this directory
-    graph_dir = '/n/coxfs01/thejohnhoffer/h5_tiles/2017_04_25'
+    graph_dir = 'results/2017_04_25'
 
     # Load one case
     def load_case(in_file):
-        with open(in_file,'r') as fd:
+        in_path = os.path.join(graph_dir, in_file)
+        with open(in_path,'r') as fd:
             return json.load(fd)
     # Load all the cases
     cases = map(load_case, glob1(graph_dir,'*.json'))
@@ -22,18 +24,25 @@ if __name__ == '__main__':
     # Plot Mbps over file x dimension
     #####
     # Fix a tile shape
-    tile_shape = [1, 32, 32]
+    tile_shape = np.uint32([1, 32, 32])
     tile_x = tile_shape[-1]
     # Set up the plot
     fig, ax = plt.subplots()
-    # Limit the Ge
     # Get a size,rate pair
     def get_pair(d):
         return map(d.get, ['file_x', 'mbps'])
+    # Limit to cases with same tile_shape
+    def is_tile(d):
+        d_tile = d['tile_shape']
+        return np.any(tile_shape == d_tile)
+    # Filter all cases for same tile_shape
+    tile_cases = filter(is_tile, cases)
     # Get the size,rate pairs for all cases
     tile_rates = map(get_pair, tile_cases)
-    # Plot list of rates over list of file sizes
-    ax.plot( *zip(*tile_rates) )
+    all_tr = np.uint32(tile_rates)
+    # Plot the lists sorted by file sizes
+    tr_order = all_tr.T[0].argsort()
+    ax.plot(*all_tr[tr_order].T)
 
     # Label the graph
     plt.ylabel('Speed (MiB per second)')
@@ -42,8 +51,8 @@ if __name__ == '__main__':
     sub_fmt = 'in blocks of {}x{}x{} pixels'
     # Make a big title with a subtitle
     small = dict(fontsize=14)
-    big = dict(y=1.05, fontsize=18)
-    plt.title(sub_fmt.format(*tile_shape), **small)
+    big = dict(fontsize=18)
     plt.suptitle(title_fmt.format(*full_shape), **big)
+    plt.title(sub_fmt.format(*tile_shape), **small)
     # Write to file
     plt.savefig('out.png')
