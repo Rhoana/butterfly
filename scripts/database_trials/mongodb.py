71d3d6d7c0d845ba5578a257d590346936258953
thejohnhoffer <hoffexi@gmail.com>
2017-05-23 16:08:47 -0400

    finish mongodb test

--- scripts/database_trials/mongodb.py
+++ scripts/database_trials/mongodb.py
@@ -1,19 +1,33 @@
 import os
 import json
-import pymongo
 import numpy as np
+from pymongo import MongoClient
+from pymongo import ASCENDING
 
 class MongoDB():
-    def __init__(self, file_name):
-        pass
+    def __init__(self, port):
+        # Connect to the mongo client
+        self.client = MongoClient('localhost', port)
+        # Create or open the experiment database
+        self.database = self.client['experiment']
+        # Create or open the synapse_keypoints collection
+        self.collection = self.database['synapse_keypoints']
 
-    def add_point(self, izyx):
-        # Turn the point to a rectangle
-        entry = np.tile(izyx[1:],2)
-        # Separate the index
-        index = izyx[0]
-        # Insert the point into the tree
-        #self._db.insert(index, entry)
+    def add_points(self, entries):
+        # Map all the entries to a dictionary
+        pairs = enumerate(entries)
+        dicts = [dict(zyx=v.tolist(), id=i) for i,v in pairs]
+        # Create the synapse index
+        id_index = [('id', ASCENDING)]
+        self.collection.create_index(id_index, unique=True)
+        # Add all the dictionaries to the collection
+        all_keys = self.collection.insert_many(dicts)
 
     def check_key(self, key):
-        pass
+        # Return the list for the given key
+        found = self.collection.find_one({'id': key})
+        return found.get('zyx') if found else []
+
+    def reset(self):
+        # Clear the collection
+        self.collection.remove()
