48253d301aee2ab9d5ad7a55883b23b298a8a83c
thejohnhoffer <hoffexi@gmail.com>
2017-05-26 12:43:43 -0400

    Add mojo filesystem

--- bfly/CoreLayer/AccessLayer/QueryLayer/ImageLayer/__init__.py
+++ bfly/CoreLayer/AccessLayer/QueryLayer/ImageLayer/__init__.py
@@ -1,9 +1,10 @@
 # Get all the classes
 from Datasource import Datasource
 from TiffGrid import TiffGrid
+from Mojo import Mojo
 from HDF5 import HDF5
 
 # Get all the derived classes
-__all__ = ['TiffGrid', 'HDF5']
+__all__ = ['TiffGrid', 'HDF5', 'Mojo']
 # Get the base class
 __all__ += ['Datasource']
