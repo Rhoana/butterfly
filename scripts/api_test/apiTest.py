9eeb41b873a7c52b64196acff6d4d5b3380fad07
thejohnhoffer <hoffexi@gmail.com>
2017-05-26 10:13:46 -0400

    remove old code from testing script

--- scripts/api_test/apiTest.py
+++ scripts/api_test/apiTest.py
@@ -1,10 +1,10 @@
 import json
 
 # For python 2
-#import urllib2 as urllib
+import urllib2 as urllib
 
 # For python 3
-from urllib import request as urllib
+#from urllib import request as urllib
 
 def format_url(server, full_size, group_list):
 
@@ -28,14 +28,6 @@ def get_synapse_keypoint(_fmt, _id):
     # Get neurons linked to a neuron
     linked_string = urllib.urlopen(_fmt.format('synapse_keypoint', _id)).read()
     return json.loads(linked_string)
-
-def get_deep_neurons(_deep_file, _neurons):
-
-    # Load deep ids from a file
-    with open(_deep_file, 'r') as jf:
-        deep_ids = set(json.load(jf))
-    # Find the union of deep ids with all neurons
-    return _neurons & deep_ids
 
 def get_shared_synapse(_syn_file):
 
@@ -70,34 +62,3 @@ def get_shared_synapse(_syn_file):
     with open('out.json','w') as jf:
         json.dump(all_shared, jf)
 
-def get_deep_links(_deep_file):
-
-    # Declare all parameters
-    full_size= [3394, 26624, 26624]
-    server = 'http://x.hoff.in:2016'
-    group_list = [
-        'team1_waypoint_201610_full_May6',
-        '25k_201610_dataset',
-        'em',
-    ]
-    # Set of deep links
-    all_deep_links = set()
-    # Get the format for requests
-    fmt = format_url(server, full_size, group_list)
-    # Get all the neurons in the volume
-    neurons = get_all_neurons(fmt)
-    # Join those with deep IDS from a file
-    deep_neurons = get_deep_neurons(_deep_file, neurons)
-    # Loop through all the deep neurons
-    for dn in deep_neurons:
-        # Get the neurons linked to this neuron
-        linked_neurons = get_neuron_children(fmt, dn)
-        # Get only linked neurons that are deep neurons
-        deep_linked_neurons = linked_neurons & deep_neurons
-        # Add linked all links to a set
-        for dln in deep_linked_neurons:
-            deep_link = frozenset([dn, dln])
-            all_deep_links.add(deep_link)
-    # Return all pairs of deep synapses
-    all_deep_pairs = [list(link) for link in all_deep_links]
-    return all_deep_pairs
