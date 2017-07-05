38b61afe7fac83dcf509a659063340cb146450a0
John Hoffer <thejohnhoffer@coxgpu01.rc.fas.harvard.edu>
2017-07-05 10:33:39 -0400

    final graph_data.py

--- scripts/h5_tile_test/graph_data.py
+++ scripts/h5_tile_test/graph_data.py
@@ -8,7 +8,7 @@ import time
 import json
 import os
 
-PLOT = 3
+PLOT = 1
 
 if __name__ == '__main__':
 
@@ -29,6 +29,7 @@ if __name__ == '__main__':
     t0 = trials[0]
     full_shape = t0['full_shape']
     # Get all file and tile shapes in X
+    files_c = np.uint64(t0['n_files'])
     files_x = np.uint64(t0['file_shape']).T[-1]
     tiles_x = np.uint64(t0['tile_shape']).T[-1]
     # Average the trials
@@ -88,18 +89,28 @@ if __name__ == '__main__':
     elif PLOT == 1:
         ##### 
         # Plot rate by h5 file width
-        # Plot lines for all tile widths
+        # Plot one line for max tile widths
         #####
-        # Get the file counts from file sizes
-        files_x = np.prod(full_shape) / np.square(files_x)
 
         # Set up the plot
-        fig, ax = plt.subplots(figsize=(8, 5))
-        for tile_i, tile_x in enumerate(tiles_x):
+        fig, ax = plt.subplots(figsize=(8, 4))
+        # Get corresponding indices
+        tiles_i = enumerate(tiles_x)
+        tiles_i = [(ti, t) for ti, t in tiles_i if t in files_x]
+        ft = [(list(files_x).index(t),ti) for ti, t in tiles_i]
+        # Get all rates
+        ft_rate = [mean_speeds[fi, ti] for fi, ti in ft]
+
+        # Reverse tile sizes for legend clarity
+        reverse_tiles = list(enumerate(tiles_x))[::-1]
+
+        for tile_i, tile_x in reverse_tiles:
             # Get rates for given tile shape
             tile_rates = mean_speeds[:, tile_i]
+            tile_dots = all_speeds[:,:, tile_i]
 
             if tile_x > 4096 or tile_x < 512:
+                #pass
                 continue
 
             # Find only where values are nonzero
@@ -108,13 +119,24 @@ if __name__ == '__main__':
                 continue
 
             # Filter tiles_x and rates by nonzero
-            real_files_x = files_x[real_rates]
+            real_files_c = files_x[real_rates]
             real_files_r = tile_rates[real_rates]
 
-            # Plot the rates for this tile shape
-            tile_label = '{}px tile size'.format(tile_x)
-            ax.plot(real_files_x, real_files_r, marker='o', label=tile_label)
+            # Scatter plot prep
+            real_dots = tile_dots.T[real_rates].T
 
+            real_err = np.std(real_dots, 0)
+
+            # Plot the rates for this tile shape
+            tile_label = '{} pixel tiles'.format(tile_x)
+            ax.errorbar(real_files_c, real_files_r, real_err, label=tile_label, marker='o')
+
+       
+        # Plot the rates for the maximum tile shapes by file size
+        tile_label = 'one tile per file'
+        ax.plot(files_x[:-3], ft_rate[:-3], label=tile_label, color='black', ls='--')
+
+        plt.ylim(0,550)
         # Power of 2 X axis
         ax.set_xscale('log', basex=2)
     #    ax.set_yscale('log', basey=2)
@@ -122,22 +144,28 @@ if __name__ == '__main__':
         ax.yaxis.set_major_formatter(FormatStrFormatter('%d'))
 
         # Set the legend fontsize
-        legend = ax.legend(loc='upper left')
+        legend = ax.legend(loc='upper right')
         for label in legend.get_texts():
             label.set_fontsize('large')
 
         # Force only relevant ticks
-        plt.xticks(files_x[:-2]) 
+        def ticker(x):
+            s = 's' if x[1] != 1 else ''
+            return "{1}\n({2} file{0})".format(s,*x)
+
+        file_size_count = zip(files_x,files_c)
+        plt.xticks(files_x, map(ticker, file_size_count)) 
+        #ax.set_xlim(xmax = files_c[-3]) 
 
         # Label the graph
         plt.ylabel('Speed (MiB per second)')
-        plt.xlabel('Number of files')
-        title_fmt = 'Rate to load full section'
+        plt.xlabel('Stored file size in pixels\n(total number of files)')
+        title_fmt = 'Rate to read tile sizes from file sizes'
         # Make a big title
         title_font = dict(fontsize=18)
         plt.title(title_fmt, **title_font)
         # Write to file
-        plt.savefig('out.pdf')
+        plt.savefig('out.pdf',bbox_inches='tight')
 
     elif PLOT == 2:
         ##### 
@@ -160,6 +188,7 @@ if __name__ == '__main__':
         for tile_i, tile_x in reverse_tiles:
             # Get rates for given tile shape
             tile_rates = mean_speeds[:, tile_i]
+            tile_dots = all_speeds[:,:, tile_i]
 
             if tile_x > 4096 or tile_x < 512:
                 #pass
@@ -174,15 +203,21 @@ if __name__ == '__main__':
             real_files_c = files_c[real_rates]
             real_files_r = tile_rates[real_rates]
 
+            # Scatter plot prep
+            real_dots = tile_dots.T[real_rates].T
+
+            real_err = np.std(real_dots, 0)
+
             # Plot the rates for this tile shape
             tile_label = '{} pixel tiles'.format(tile_x)
-            ax.plot(real_files_c, real_files_r, label=tile_label, marker='o')
+            ax.errorbar(real_files_c, real_files_r, real_err, label=tile_label, marker='o')
 
        
         # Plot the rates for the maximum tile shapes by file size
         tile_label = 'one tile per file'
-        ax.plot(ft_count[:-3], ft_rate[:-3], label=tile_label, color='black', ls='--')
+        ax.plot(files_c[:-3], ft_rate[:-3], label=tile_label, color='black', ls='--')
 
+        plt.ylim(0,550)
         # Power of 2 X axis
         ax.set_xscale('log', basex=2)
     #    ax.set_yscale('log', basey=2)
@@ -201,7 +236,7 @@ if __name__ == '__main__':
         # Label the graph
         plt.ylabel('Speed (MiB per second)')
         plt.xlabel('Number of files')
-        title_fmt = 'Bit rate to read files'
+        title_fmt = 'Rate to read multiple tiles per file'
         # Make a big title
         title_font = dict(fontsize=18)
         plt.title(title_fmt, **title_font)
@@ -245,15 +280,15 @@ if __name__ == '__main__':
 
             # Scatter plot prep
             real_dots = tile_dots.T[real_rates][:4].T
-            real_dot_ratios = np.uint64([real_ratios,]*len(real_dots))
 
-            dot_color = 'crgb'[tile_i]
-            ax.scatter(real_dot_ratios[:], real_dots[:], s=4, color=dot_color)
+            real_err = np.std(real_dots, 0)
 
             # Plot the rates for this tile shape
             tile_label = '{} pixel tiles'.format(tile_x)
-            ax.plot(real_ratios, real_times, label=tile_label, marker='o')
+            ax.errorbar(real_ratios, real_times, real_err, label=tile_label, marker='o')
 
+        plt.xlim(xmin=1-0.125)
+        plt.ylim(0,550)
         # Power of 2 X axis
         ax.set_xscale('log', basex=2)
     #    ax.set_yscale('log', basey=2)
